7b5329a5ea4dce841c5b17b23c2f6a42a6619c96
Poolay <huangmichelpro@gmail.com>
2024-06-29 20:17:21 +0200

    small fixes

--- NLP/LoRA/train.sh
+++ NLP/LoRA/train.sh
@@ -1,4 +1,8 @@
 
+# debug
+python train.py --yaml_config debugcfg.yaml --log_dir debug_logs
+
+# real train
 python train.py --yaml_config cfg.yaml --log_dir logs
 
 python LoRA/train.py --yaml_config LoRA/cfg.yaml --log_dir LoRA/logs
