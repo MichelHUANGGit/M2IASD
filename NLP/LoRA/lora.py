small fixes, infinite loader, alpha scaling

NLP/LoRA/lora.py
@@ -11,8 +11,6 @@ class LoRA_Linear(nn.Module):
         super().__init__()
         self.base_layer = base_layer
         self.r = r
-        self.alpha = alpha
-        self.scaling = alpha / r
         self.base_layer.weight.requires_grad = False
         self.output_dim, self.input_dim = base_layer.weight.shape
 
@@ -26,7 +24,7 @@ class LoRA_Linear(nn.Module):
         # print("B", self.B.weight.shape)
         # print("A", self.A.weight.shape)
         y = self.base_layer(x)
-        y += (self.A(self.B(x)) * self.scaling)
+        y += (self.A(self.B(x)))
         return y
     
 def count_parameters(model):

NLP/LoRA/train.py
@@ -1,5 +1,5 @@
 from lora import apply_LoRA_tinyllama, configure_optimizers, save_AB_weights_tinyllama
-from data_utils import DataCollator, get_tokenizer, preprocess_fn
+from data_utils import DataCollator, get_tokenizer, preprocess_fn, CustomDataCollator, CustomDataLoader
 
 import torch
 import torch.nn as nn
@@ -20,11 +20,13 @@ class TrainingCfg:
     precision:str
     batch_size:int
     grad_accum_steps:int
-    epochs:int
+    epochs:float
     warmup_epochs:float
+    epoch_per_eval:float
     use_compile:bool
     use_autocast:bool
     weight_decay:float
+    use_lr_scheduler:bool
     max_lr:float
     min_lr:float
     beta1:float
@@ -64,6 +66,33 @@ def log_results(csv_file, metrics_list):
     with open(csv_file, "a", newline='') as file:
         writer = csv.writer(file)
         writer.writerow(metrics_list)
+
+def evaluate(model, loader:CustomDataLoader, loss_fn:nn.Module, use_autocast=True, device_type="cuda"):
+    model.eval()
+    val_loss = 0.
+    correct = 0
+    processed = 0
+    t0 = time()
+    with torch.no_grad():
+        for i in range(1, len(loader)+1):
+            batch = loader.next_batch()
+            input_ids, attention_mask = batch["input_ids"], batch["attention_mask"]
+            if use_autocast:
+                with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+                    logits = model(input_ids, attention_mask, use_cache=False)["logits"]
+            else:
+                logits = model(input_ids, attention_mask, use_cache=False)["logits"]
+            labels = batch["labels"]
+            lmask = batch["loss_mask"]
+            loss = loss_fn(logits[lmask], labels)
+            val_loss = (val_loss * (i-1) + loss.item()) / i
+            correct += torch.sum(logits[lmask].argmax(dim=1) == labels).item()
+            processed += len(labels)
+            acc = correct / processed
+            dt = time() - t0
+            # validation dataset-wise metrics updated sequentially
+            print(f"acc: {acc*100:.2f}% | loss:{val_loss:.4f} | eval dt: {dt:.4f}")
+    return acc, val_loss, dt
     
 def train():
     #Config stuff
@@ -121,45 +150,51 @@ def train():
     #dataset
     print("Loading dataset...")
     dataset = load_dataset("tuetschek/e2e_nlg")
-    print("Preprocessing the dataset:")
+    print("Preprocessing the dataset...")
     dataset = dataset.map(preprocess_fn, fn_kwargs={"tokenizer":tokenizer})
-    data_collator = DataCollator(pad_token_id=tokenizer.pad_token_id, max_length=train_cfg.max_length, device=device)
-    train_loader = DataLoader(dataset["train"], batch_size=train_cfg.batch_size, collate_fn=data_collator) # type: ignore
-    val_loader = DataLoader(dataset["validation"], batch_size=train_cfg.batch_size, collate_fn=data_collator) # type: ignore
+    # data_collator = DataCollator(pad_token_id=tokenizer.pad_token_id, max_length=train_cfg.max_length, device=device)
+    # train_loader = DataLoader(dataset["train"], batch_size=train_cfg.batch_size, collate_fn=data_collator) # type: ignore
+    # val_loader = DataLoader(dataset["validation"], batch_size=train_cfg.batch_size, collate_fn=data_collator) # type: ignore
+    collate_fn = CustomDataCollator(pad_token_id=tokenizer.pad_token_id, max_length=train_cfg.max_length, device=device)
+    train_loader = CustomDataLoader(dataset["train"], batch_size=train_cfg.batch_size, collate_fn=collate_fn) # type: ignore
+    val_loader = CustomDataLoader(dataset["validation"], batch_size=train_cfg.batch_size, collate_fn=collate_fn) # type: ignore
 
     # Learning rate scheduling
     N = len(dataset["train"]) # type: ignore
-    warmup_steps = train_cfg.warmup_epochs * N / (train_cfg.batch_size * train_cfg.grad_accum_steps)
-    max_steps = train_cfg.epochs * N / (train_cfg.batch_size * train_cfg.grad_accum_steps)
-    print(f"warmup steps: {warmup_steps} | max_steps {max_steps}")
+    steps_per_epoch = N / (train_cfg.batch_size * train_cfg.grad_accum_steps)
+    warmup_steps = train_cfg.warmup_epochs * steps_per_epoch
+    max_steps = round(train_cfg.epochs * steps_per_epoch)
+    steps_per_eval = round(train_cfg.epoch_per_eval * steps_per_epoch)
+    print(f"warmup steps: {warmup_steps:.2f} | max_steps {max_steps} | Steps per epoch: {steps_per_epoch:.2f} | Steps per eval: {steps_per_eval:.2f}")
 
     def get_lr(step):
-        '''from https://github.com/karpathy/build-nanogpt/blob/master/train_gpt2.py'''
-        # 1) linear warmup for warmup_iters steps
-        if step < warmup_steps:
-            return train_cfg.max_lr * step / warmup_steps
-        # 2) if it > lr_decay_iters, return min learning rate
-        if step > max_steps:
-            return train_cfg.min_lr
-        # 3) in between, use cosine decay down to min learning rate
-        decay_ratio = (step - warmup_steps) / (max_steps - warmup_steps)
-        assert 0 <= decay_ratio <= 1
-        coeff = 0.5 * (1.0 + cos(pi * decay_ratio)) # coeff starts at 1 and goes to 0
-        return train_cfg.min_lr + coeff * (train_cfg.max_lr - train_cfg.min_lr)
+        if train_cfg.use_lr_scheduler:
+            '''from https://github.com/karpathy/build-nanogpt/blob/master/train_gpt2.py'''
+            # 1) linear warmup for warmup_iters steps
+            if step < warmup_steps:
+                return train_cfg.max_lr * step / warmup_steps
+            # 2) after, use cosine decay down to min learning rate
+            decay_ratio = (step - warmup_steps) / (max_steps - warmup_steps)
+            assert 0 <= decay_ratio <= 1
+            coeff = 0.5 * (1.0 + cos(pi * decay_ratio)) # coeff starts at 1 and goes to 0
+            return train_cfg.min_lr + coeff * (train_cfg.max_lr - train_cfg.min_lr)
+        else:
+            return train_cfg.max_lr
     
-
     print("Begin Training")
     print("="*100)
     start = time()
     step = 0
     tokens_processed = 0
-    for epoch in range(1, train_cfg.epochs+1):
+    last_tokens_processed = tokens_processed
+    for step in range(1, max_steps+1):
         model.train()
-        last_tokens_processed = tokens_processed
         train_loss = 0.
         t0 = time()
         optimizer.zero_grad()
-        for it, batch in enumerate(train_loader, start=1):
+        # gradient accumulation loop: perform grad_accum_steps loss.backward() before doing optimizer.step()
+        for i in range(1, train_cfg.grad_accum_steps+1):
+            batch = train_loader.next_batch()
             input_ids, attention_mask = batch["input_ids"], batch["attention_mask"]
             if train_cfg.use_autocast:
                 with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
@@ -169,64 +204,24 @@ def train():
             labels = batch["labels"]
             loss = loss_fn(logits[batch["loss_mask"]], labels) / train_cfg.grad_accum_steps
             loss.backward()
-
             train_loss += loss.item()
             tokens_processed += labels.size(0)
-            modulo = it % train_cfg.grad_accum_steps
-            if modulo == 0:
-                step += 1
-                norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=train_cfg.max_grad_norm).item()
-                lr = get_lr(step)
-                optimizer.step()
-                optimizer.zero_grad()
-                dt = time()-t0
-                tok_per_sec = (tokens_processed - last_tokens_processed) / dt
-                # step-wise metrics (not micro batch-wise)
-                print(f"Epoch: {epoch:2d} | Step: {step:4d} | tokens processed {tokens_processed:8d} | loss:{train_loss:.4f} | lr:{lr:.6f} | grad norm: {norm:.4f} | step dt: {dt:.4f}s | tok/sec {tok_per_sec:.4f}")
-                log_results(train_csv_file, [epoch, step, tokens_processed, train_loss, lr, norm, dt, tok_per_sec])
-                t0 = time()
-                last_tokens_processed = tokens_processed
-                # import code; code.interact(local=locals())
-        # optimizer.step the remaining steps
-        if modulo != 0:
-            step += 1
-            norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=train_cfg.max_grad_norm)
-            # if modulo == 1 and grad_accum_steps = 8, we did .backward() once, but the loss is divided by 8 instead of 1
-            # Therefore we should rescale the lr by 8/1 = 1
-            # if modulo == 3 and grad_accum_steps = 8, we did .backward() 3 times, but the loss is divided by 8 instead of 3
-            # Therefore we should rescale the lr by 8/3 = 2.67 etc...
-            lr_rescaler = train_cfg.grad_accum_steps / modulo
-            lr = get_lr(step) * lr_rescaler
-            optimizer.step()
-            optimizer.zero_grad()
-            dt = time() - t0
-            tok_per_sec = (tokens_processed - last_tokens_processed) / dt
-            print(f"Epoch: {epoch:2d} | Step: {step:4d} | tokens processed {tokens_processed:8d} | loss:{train_loss:.4f} | lr:{lr:.6f} | grad norm: {norm:.4f} | step dt: {dt:.4f}s | tok/sec {tok_per_sec:.4f}")
-            log_results(train_csv_file, [epoch, step, tokens_processed, train_loss, lr, norm, dt, tok_per_sec])
-        
-        model.eval()
-        val_loss = 0.
-        correct = 0
-        processed = 0
+        norm = torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=train_cfg.max_grad_norm).item()
+        lr = get_lr(step) * model_cfg.alpha / model_cfg.r
+        optimizer.step()
+        optimizer.zero_grad()
+        dt = time()-t0
+        tok_per_sec = (tokens_processed - last_tokens_processed) / dt
+        epoch = step/steps_per_epoch
+        # step-wise metrics
+        print(f"Epoch: {epoch:2f} | Step: {step:4d} | tokens processed {tokens_processed:8d} | loss:{train_loss:.4f} | lr:{lr:.6f} | grad norm: {norm:.4f} | step dt: {dt:.4f}s | tok/sec {tok_per_sec:.4f}")
+        log_results(train_csv_file, [epoch, step, tokens_processed, train_loss, lr, norm, dt, tok_per_sec])
+        last_tokens_processed = tokens_processed
         t0 = time()
-        with torch.no_grad():
-            for it, batch in enumerate(val_loader, start=1):
-                input_ids, attention_mask = batch["input_ids"], batch["attention_mask"]
-                if train_cfg.use_autocast:
-                    with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
-                        logits = model(input_ids, attention_mask, use_cache=False)["logits"]
-                else:
-                    logits = model(input_ids, attention_mask, use_cache=False)["logits"]
-                labels = batch["labels"]
-                lmask = batch["loss_mask"]
-                loss = loss_fn(logits[lmask], labels)
-                val_loss = (val_loss * (it-1) + loss.item()) / it
-                correct += torch.sum(logits[lmask].argmax(dim=1) == labels).item()
-                processed += len(labels)
-                acc = correct / processed
-                dt = time() - t0
-                # dataset-wise metrics updated sequentially
-                print(f"Epoch: {epoch}| Step: {step:4d} | acc: {acc*100:.2f} | loss:{val_loss:.4f} | eval dt: {dt:.4f} | tokens processed {processed:4d}")
+        # import code; code.interact(local=locals())
+
+        if step % steps_per_eval == 0:
+            acc, val_loss, dt = evaluate(model, val_loader, loss_fn, train_cfg.use_autocast, device_type)
             log_results(val_csv_file, [epoch, step, tokens_processed, acc, val_loss, dt])
     
     end = time() - start