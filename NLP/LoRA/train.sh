83c3646ae3e2af1f4c584a6fdcaecb355d0b8253
Poolay <huangmichelpro@gmail.com>
2024-06-30 16:00:36 +0200

    added evaluation command

--- NLP/LoRA/train.sh
+++ NLP/LoRA/train.sh
@@ -6,3 +6,7 @@ python train.py --yaml_config debugcfg.yaml --log_dir debug_logs
 python train.py --yaml_config cfg.yaml --log_dir logs
 
 python LoRA/train.py --yaml_config LoRA/cfg.yaml --log_dir LoRA/logs
+
+
+# evaluation
+python evaluate.py --run_path logs/2024-06-29/run3 --split test
